7793976628f6e9278040e7feede7a44dbecb8f46
ajay_gupta0512 <ajay_gupta0512@hotmail.com>
2015-03-22 11:23:51 -0500

    Added 7.Generate a target value using the random number generator. 8.Add logic to start a new round in the game and then invoke it from view loading and click of “Hit Me” button. 9.Add logic to update the target label based on the target value generated from the random number generator.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 50
+    var targetValue: Int = 0
+    var roundNumber: Int = 0
     
+    @IBOutlet weak var targetLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        slider.value = 50
+        targetValue = Int(arc4random_uniform(100)) + 1
+        roundNumber++
+        
+        targetLabel.text = String(targetValue)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,16 +36,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The current value of slider: \(currentValue)"
+        let message = "The current value of slider: \(currentValue)" +
+                      "\n The target value is: \(targetValue)"
          let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: false, completion: nil)
+        
+        startNewRound()
     }
 
     @IBAction func sliderMoved(sender: AnyObject) {
         let slider = sender as UISlider
-        println("Value of slider now is: \(slider.value)")
         currentValue = lroundf(slider.value)
     }
 }
